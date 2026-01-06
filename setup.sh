02285ce4957c31364834bc87b1f38f1ab08f777c
Glaaj <glenneggen@outlook.com>
2026-01-06 14:48:43 +0100

    add docker setup steps to script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,10 @@ yay -S --needed - <package_list.txt
 # run stow to create symlinks
 stow . --adopt --target ~ --verbose 1
 git reset .
+
+# add user to docker group
+sudo usermod -aG docker $USER
+
+# enable and start docker service
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
