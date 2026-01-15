f3dd55e23a103ba5f590f5421b400bed033c7c16
Glaaj <glenneggen@outlook.com>
2026-01-15 09:39:22 +0100

    increase bootstrap speed

--- setup.sh
+++ setup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# Use all CPU cores for AUR builds to speed compilation
+MAKEFLAGS="-j$(nproc)"
+export MAKEFLAGS
+
+sudo pacman -Sy
+
 #install_packages
-yay -S --needed - <package_list.txt
+yay -S --needed --noconfirm - <package_list.txt
 
 # run stow to create symlinks
 stow . --adopt --target ~ --verbose 1
 git reset .
 
 # add user to docker group
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 
 # enable and start docker service
 sudo systemctl enable docker.service
